24ac837b7e4133b9a93d6ee710eba4c60b267e94
D.J.Krol.1@student.rug.nl <s3142221@peregrine.hpc.rug.nl>
2021-04-07 10:31:42 +0200

    sbatch now using n=50

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
+#SBATCH --time=20:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
 #SBATCH --mem=10GB
-#SBATCH --output=comsoc.out
+#SBATCH --output=comsoc_n50.out
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE
 #SBATCH --mail-user=d.j.krol.1@student.rug.nl
 
@@ -11,6 +11,7 @@
 module load TensorFlow/2.3.1-fosscuda-2019b-Python-3.7.4
 pip install pycuda --user
 pip install scikit-learn --user
+pip install tensorflow_addons --user
 
 #Run the training
-python main.py -n 20 -e 5
+python main.py -n 50 -e 5
